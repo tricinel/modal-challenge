b4ca8f4d4da1950136537c4bd1d39cccf4d3b87b
Bogdan Lazar <bogdan@bogdanlazar.com>
2021-02-01 22:55:07 +0100

    Add Icons

---
+++ src/icons/index.ts
@@ -0,0 +1,7 @@
+export { default as BellIcon } from './BellIcon';
+export { default as CheckIcon } from './CheckIcon';
+export { default as CloseIcon } from './CloseIcon';
+export { default as EmbedIcon } from './EmbedIcon';
+export { default as InfoIcon } from './InfoIcon';
+export { default as LinkIcon } from './LinkIcon';
+export { default as WarningIcon } from './WarningIcon';
