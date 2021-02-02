d9f8246cf741e8f074dfa9c6f26f086a01620dff
Bogdan Lazar <bogdan@bogdanlazar.com>
2021-02-02 09:59:10 +0100

    Improve default button styling

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,6 +2,8 @@ import styled from '@emotion/styled';
 import { colors, radii, fontSizes } from '../../tokens';
 
 const ButtonBox = styled('button')`
+  align-items: center;
+  appearance: none;
   background-color: ${colors['neutral-200']};
   border-color: ${colors['neutral-300']};
   border-style: solid;
@@ -9,12 +11,19 @@ const ButtonBox = styled('button')`
   border-radius: ${radii.xs};
   box-sizing: border-box;
   color: ${colors['white-100']};
-  display: inline-block;
+  display: inline-flex;
   font-size: ${fontSizes.base};
   line-height: 1.32;
+  margin: 0;
+  outline: 0;
   text-align: center;
   text-decoration: none;
   user-select: none;
+
+  &::after,
+  &::before {
+    box-sizing: border-box;
+  }
 
   &:hover:not(:disabled),
   &:active:not(:disabled) {
