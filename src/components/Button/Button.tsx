ff88f0e6f9c63de6d5ed46d8adaed10eebc0adc9
Bogdan Lazar <bogdan@bogdanlazar.com>
2021-02-02 12:46:14 +0100

    Make Button extend from Box

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,29 +1,22 @@
 import styled from '@emotion/styled';
+import Box from '../Box';
 import { colors, radii, fontSizes } from '../../tokens';
 
-const ButtonBox = styled('button')`
+const ButtonBox = styled(Box.withComponent('button'))`
   align-items: center;
-  appearance: none;
   background-color: ${colors['neutral-200']};
   border-color: ${colors['neutral-300']};
   border-style: solid;
   border-width: 1px;
   border-radius: ${radii.xs};
-  box-sizing: border-box;
   color: ${colors['white-100']};
   display: inline-flex;
   font-size: ${fontSizes.base};
   line-height: 1.32;
   margin: 0;
-  outline: 0;
   text-align: center;
   text-decoration: none;
   user-select: none;
-
-  &::after,
-  &::before {
-    box-sizing: border-box;
-  }
 
   &:hover:not(:disabled),
   &:active:not(:disabled) {
