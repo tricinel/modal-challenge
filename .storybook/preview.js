98ac6cafb1461ff6e1f63dee48fe2a556a53418c
Bogdan Lazar <bogdan@bogdanlazar.com>
2021-02-03 08:49:48 +0100

    Re-arrange stories for Modal component

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,5 +2,10 @@
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   viewMode: 'docs',
-  previewTabs: { 'storybook/docs/panel': { index: -1 } }
+  previewTabs: { 'storybook/docs/panel': { index: -1 } },
+  options: {
+    storySort: {
+      order: ['Introduction', 'Components']
+    }
+  }
 }
